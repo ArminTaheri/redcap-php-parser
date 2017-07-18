0ba3db31606b61b3be4f303757ff50296ddbba48
ArminTaheri <armin.ytaheri@gmail.com>
2017-07-18 00:33:43 -0400

    finish lexer, start parser

--- lexer.php
+++ lexer.php
@@ -30,17 +30,36 @@
             "/^\"[^\"]*\"/" => "ESTRING",
             "/^\'[^\']*\'/" => "STRING",
             "/^\[/" => "[",
-            "/^\]/" => "]"
+            "/^\]/" => "]",
+            "/^[\t ]*/" => null, // Skip spaces and tabs
         );
-        function match($expression, $offset) {
-
+        static function match($expression, $offset) {
+            $substr = substr($expression, $offset);
+            foreach(static::$terminals as $pattern => $token) {
+                if (preg_match($pattern, $substr, $matches)) {
+                    return array(
+                        'match' => $matches[0],
+                        'token' => $token,
+                    );
+                }
+            }
+            return false;
         }
-        function lex($expression) {
+        static function lex($expression) {
             $tokens = array();
             $offset = 0;
             while($offset < strlen($expression)) {
-
+                $matched = static::match($expression, $offset);
+                if ($matched === false) {
+                    throw new Exception("Unexpected token after: " . substr($expression, $offset));
+                }
+                // Skip spaces and tabs
+                if ($matched['token'] !== null) {
+                    $tokens[] = $matched;
+                }
+                $offset += strlen($matched['match']);
             }
+            return $tokens;
         }
     }
 ?>
--- parser.php
+++ parser.php
@@ -1,7 +1,100 @@
 <?php
+    include "lexer.php";
+
+    $opPrecs = array(
+        array("left", 2, "and", "or"),
+        array("left", 2, "=", "<", ">", "<>", "<=", ">="),
+        array("left", 2, "+", "-"),
+        array("left", 2, "*", "/"),
+        array("left", 2, "^"),
+        array("right", 1, "not"),
+        array("left", 1, "!"),
+        array("left", 1, "%"),
+        array("right", 1, "-"),
+    );
     class Parser {
-        function parseExpr() {
-            
+        function __construct($expression) {
+            $this->$expression = $expression;
+            $this->$tokens = Lexer.lex($expression);
+            $this->$offset = -1;
+        }
+        function parse() {
+            return $this->parseExpression();
+        }
+        function next() {
+            $this-$offset += 1;
+            if ($this->$offset > count($this->$tokens)) {
+                throw new Exception("Invalid expression: " . $this->$expression);
+            }
+        }
+        function expect($token) {
+            $currentTok = $this->$tokens[$this->offset];
+            if ($currentTok['token'] !== $token) {
+                return false;
+            }
+            return $currentTok['match'];
+        }
+        function parseExpression() {
+            $left = $this->parseBoolComp();
+            $op = $this->$expect('and') || $this->$expect('or');
+            $this->next();
+            if ($op === false) {
+                return $left;
+            }
+            $right = $this->parseExpression();
+            return array(
+                'type' => 'BinaryOp',
+                'op' => $op,
+                'args' => array($left, $right),
+            );
+        }
+        function parseBoolComp() {
+            $left = $this->parseNumTerm();
+            $filt = array_filter("=", "<", ">", "<>", "<=", ">=", $this->expect);
+            $this->next();
+            if ($filt.length === 0) {
+                return false;
+            }
+            $op = $filt[0];
+            if ($op === false) {
+                return $left;
+            }
+            $right = $this->parseBoolComp();
+            return array(
+                'type' => 'BinaryOp',
+                'op' => $op,
+                'args' => array($left, $right),
+            );
+        }
+        function parseNumTerm() {
+            $left = $this->parseNumFactor();
+            $filt = array_filter("+", "-", $this->expect);
+            $this->next();
+            if ($filt.length === 0) {
+                return false;
+            }
+            $op = $filt[0];
+            $right = $this->parseNumTerm();
+            return array(
+                'type' => 'BinaryOp',
+                'op' => $op,
+                'args' => array($left, $right),
+            );
+        }
+        function parseNumFactor() {
+            $left = $this->parseNumFactor();
+            $filt = array_filter("+", "-", $this->expect);
+            $this->next();
+            if ($filt.length === 0) {
+                return false;
+            }
+            $op = $filt[0];
+            $right = $this->parseNumTerm();
+            return array(
+                'type' => 'BinaryOp',
+                'op' => $op,
+                'args' => array($left, $right),
+            );
         }
     }
 ?>
